c571f89ec3e441be3a91cbc8a8c866df3c0abf20
Siniša Bolsšc <sinisa.bolsec0@gmail.com>
2021-02-13 19:59:27 +0100

    nav links and css

--- marioplan/src/components/layout/SignedOutLinks.js
+++ marioplan/src/components/layout/SignedOutLinks.js
@@ -4,12 +4,8 @@ import { NavLink } from 'react-router-dom';
 const SignedOutLinks = () => {
     return (
         <ul className="right">
-            <li>
-                <NavLink to="/signup">Sign up</NavLink>
-            </li>
-            <li>
-                <NavLink to="/signin">Log in</NavLink>
-            </li>
+            <li><NavLink to="/signup">Sign up</NavLink></li>
+            <li><NavLink to="/signin">Log in</NavLink></li>
         </ul>
     );
 }
